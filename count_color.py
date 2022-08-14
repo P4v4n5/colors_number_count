2596ce4c4a262d9ee55f769b11cd67350b74b509
Pavan Kumar S <53817263+P4v4n5@users.noreply.github.com>
2022-08-14 22:03:18 +0530

    Update count_color.py

--- count_color.py
+++ count_color.py
@@ -1,6 +1,7 @@
 #transak_task1
 # - Pavan Kumar S
 
+# used to convert all letters to upper case
 def case_sensitive(input_file, case_sensitive_output_file):
     fin = open(input_file, "r")
 
@@ -8,7 +9,7 @@ def case_sensitive(input_file, case_sensitive_output_file):
     for line in fin:
         fout.write(line.upper())
 
-
+# used to convert multi spaces to a single space and output them in seperate line
 def space_sensitive(case_sensitive_output_file, space_sensitive_output_file):
     fin = open(case_sensitive_output_file, "r")
     fout = open(space_sensitive_output_file, "wt")
@@ -30,6 +31,7 @@ if __name__ == '__main__':
     fin1 = space_sensitive(case_sensitive_output_file, space_sensitive_output_file)
     # fin1 = open("out.txt", "r")
 
+    # empty lists to store numbers of each color
     RED_list = []
     BLUE_list = []
     YELLOW_list = []
@@ -38,7 +40,8 @@ if __name__ == '__main__':
 
     for each_line in fin1:
         split = each_line.split(" ")
-
+        
+        # if a perticular color found then its corrsponding number is captured and appended to the list with its empty list
         if split[0] == "RED":
             RED_list.append(int(split[1]))
         elif split[0] == "BLUE":
@@ -49,13 +52,15 @@ if __name__ == '__main__':
             GREEN_list.append(int(split[1]))
         elif split[0] == "MAGENTA":
             MAGENTA_list.append(int(split[1]))
-
+    
+    #summing up all the numbers in the list.
     RED_sum = sum(RED_list)
     BLUE_sum = sum(BLUE_list)
     YELLOW_sum = sum(YELLOW_list)
     GREEN_sum = sum(GREEN_list)
     MAGENTA_sum = sum(MAGENTA_list)
-
+    
+    #output
     print("Blue ", BLUE_sum)
     print("Green ", GREEN_sum)
     print("Magenta ", MAGENTA_sum)
